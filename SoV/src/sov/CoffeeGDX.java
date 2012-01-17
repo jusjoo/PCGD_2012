e1d90945b21e89ef80d8957c0c801c053704758c
speakk <speak@gmail.com>
2012-01-17 12:38:21 +0200

    Maps are now loaded based on layer names, not on layer order.

--- SoV/src/sov/CoffeeGDX.java
+++ SoV/src/sov/CoffeeGDX.java
@@ -23,6 +23,7 @@ import com.badlogic.gdx.graphics.g2d.tiled.TiledLayer;
 import com.badlogic.gdx.graphics.g2d.tiled.TiledLoader;
 import com.badlogic.gdx.graphics.g2d.tiled.TiledMap;
 import com.badlogic.gdx.graphics.g2d.tiled.TiledObject;
+import com.badlogic.gdx.graphics.g2d.tiled.TiledObjectGroup;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
@@ -75,54 +76,56 @@ public class CoffeeGDX implements ApplicationListener {
 		tileMapRenderer = new TileMapRenderer(map, atlas, 5, 5);
 		
 		
-		int[][] tiles = map.layers.get(0).tiles;
-		
-		ArrayList<StaticTile> maptiles = new ArrayList<StaticTile>();
-		
-		for(int y=0; y<tiles.length; y++) {
-			for(int x=0; x<tiles[y].length; x++) {
-				if(tiles[y][x] != 0) {
-					String property = map.getTileProperty(tiles[y][x], "slope");
-					if(property != null && property.equals("left")) {
-						maptiles.add(new StaticTile(world, x, -y+tiles.length, StaticTile.Shape.LEFT));
-					} else if (property != null && property.equals("right")) {
-						maptiles.add(new StaticTile(world, x, -y+tiles.length, StaticTile.Shape.RIGHT));
-					} else {
-						maptiles.add(new StaticTile(world, x, -y+tiles.length, StaticTile.Shape.SQUARE));
+		for(TiledLayer layer : map.layers) {
+			
+			int[][] tiles = layer.tiles;
+			
+			if(layer.name.equals("Foreground")) {
+				
+				
+				ArrayList<StaticTile> maptiles = new ArrayList<StaticTile>();
+				
+				for(int y=0; y<tiles.length; y++) {
+					for(int x=0; x<tiles[y].length; x++) {
+						if(tiles[y][x] != 0) {
+							String property = map.getTileProperty(tiles[y][x], "slope");
+							if(property != null && property.equals("left")) {
+								maptiles.add(new StaticTile(world, x, -y+map.height, StaticTile.Shape.LEFT));
+							} else if (property != null && property.equals("right")) {
+								maptiles.add(new StaticTile(world, x, -y+map.height, StaticTile.Shape.RIGHT));
+							} else {
+								maptiles.add(new StaticTile(world, x, -y+map.height, StaticTile.Shape.SQUARE));
+							}
+							
+						}
 					}
-					
 				}
-			}
+				
+			} // END Foreground
+			
+			
 		}
 		
-		ArrayList<TiledObject> dynTiles = map.objectGroups.get(0).objects;
-		dynMapTiles = new ArrayList<MovingSprite>();
+		ArrayList<TiledObjectGroup> objectGroups = map.objectGroups;
 		
-		
-		//TextureRegion textureRegion = 
-		
-		
-		
-		
-		for(TiledObject object : dynTiles) {
-			
-			
-			HashMap<AnimatedSprite.AnimationState, Animation> tileAnimations = new HashMap<AnimatedSprite.AnimationState, Animation>();
-			ArrayList<TextureRegion> textureRegions = new ArrayList<TextureRegion>();
-			textureRegions.add(atlas.getRegion(object.gid));
-			tileAnimations.put(AnimationState.IDLE, new Animation(0.1f, textureRegions));
-			
-			dynMapTiles.add(new MovingSprite(world,
+		for(TiledObjectGroup objectGroup : objectGroups) {
+			if(objectGroup.name.equals("DynamicTiles")) {
+				ArrayList<TiledObject> dynTiles = map.objectGroups.get(0).objects;
+				dynMapTiles = new ArrayList<MovingSprite>();
+				
+				for(TiledObject object : dynTiles) {
 					
-					//new Vector2(object.x*2, -object.y*2+tiles.length), new Vector2(16f,16f),
-					//new Vector2(object.x, (-(object.y*1/16)+tiles.length)), new Vector2(16f,16f),
-					new Vector2(object.x, -object.y+(tiles.length+1)*16f), new Vector2(16f,16f),
 					
-					tileAnimations));
-					//new Vector2(object.x, object.y)));
-					//new Vector2(object.x, object.y)));
-					System.out.println("X: " + object.x + " Y: " + object.y);
-					//-(object.y*2/MovingSprite.PIXELS_PER_METER)+tiles.length)
+					HashMap<AnimatedSprite.AnimationState, Animation> tileAnimations = new HashMap<AnimatedSprite.AnimationState, Animation>();
+					ArrayList<TextureRegion> textureRegions = new ArrayList<TextureRegion>();
+					textureRegions.add(atlas.getRegion(object.gid));
+					tileAnimations.put(AnimationState.IDLE, new Animation(0.1f, textureRegions));
+					
+					dynMapTiles.add(new MovingSprite(world,
+							new Vector2(object.x, -object.y+(map.height+1)*map.tileHeight),
+							new Vector2(16f,16f),tileAnimations));
+				}
+			} // END DynamicTiles
 		}
 		
 		//int amountOfFrames = 5; 
