3c0aa343d83841ce0b562432098f6f5b731b518f
speakk <speak@gmail.com>
2012-01-26 15:55:11 +0200

    DynamicObjectFactory fleshed out. Could work! Maybe not! SIMSALABIM.

--- SoV/src/sov/DynamicObjectFactory.java
+++ SoV/src/sov/DynamicObjectFactory.java
@@ -5,10 +5,14 @@ import java.io.FileReader;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map.Entry;
 
 import sov.SpriteComponent.AnimationState;
 
 import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.JsonReader;
@@ -25,8 +29,6 @@ public class DynamicObjectFactory {
 		
 		Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
 		
-		//Type protoType = new 
-		
 		String filename = "creatures";
 		Creature[] creaturePrototypes = null;
 		try {
@@ -40,23 +42,53 @@ public class DynamicObjectFactory {
 		}
 		
 		for(Creature creaturePrototype : creaturePrototypes) {
-			//spritePrototypes.put(spritePrototype.getId(), spritePrototype);
-			//prototype.init();
-			//prototypeObjects.put(filename + String.valueOf(prototype.getID()), prototype);
-			
 			creatures.put(creaturePrototype.creatureType, creaturePrototype);
-			System.out.println(creaturePrototype.frames.get(AnimationState.Idle));
+			Texture spritesTexture = new Texture(new FileHandle("assets/creatures/" + creaturePrototype.textureName));
 			
-			//System.out.println(prototype.getClass() + " id: " + prototype.getID());
+			HashMap<SpriteComponent.AnimationState, ArrayList<Object>> animationStates =
+					creaturePrototype.frames;
 			
-			//System.out.println(prototype.getID());
+			HashMap<SpriteComponent.AnimationState, Animation> spriteAnimations = new HashMap<SpriteComponent.AnimationState, Animation>();
+			
+			
+			for(Entry<SpriteComponent.AnimationState, ArrayList<Object>> animationEntry: animationStates.entrySet()) {
+				// "Frame size, Origin offset, Start frame coordinates, Length, Speed (ms)"
+				
+				TextureRegion subRegion = new TextureRegion(spritesTexture);
+				
+				Vector2 startCoordinates = new Vector2();
+				Vector2 totalSize = new Vector2();
+				startCoordinates.x = ((int[])(animationEntry.getValue().get(2)))[0]*16;
+				startCoordinates.y = ((int[])(animationEntry.getValue().get(2)))[1]*16;
+				totalSize.x = Float.parseFloat(animationEntry.getValue().get(3).toString());
+				totalSize.y = ((int[])(animationEntry.getValue().get(0)))[1]*16;
+				
+				subRegion.setRegion(startCoordinates.x, startCoordinates.y, totalSize.x, totalSize.y);
+				
+				TextureRegion frames[][] =
+						TextureRegion.split(spritesTexture, ((int[])(animationEntry.getValue().get(0)))[0]*16,
+															((int[])(animationEntry.getValue().get(0)))[1]*16);
+				
+				ArrayList<TextureRegion> textureRegions = new ArrayList<TextureRegion>();
+				for(int y=0; y<frames.length; y++) {
+					for(int x=0; x<frames[y].length; x++) {
+						textureRegions.add(frames[y][x]);
+					}
+				}
+				
+				float frameDelay = Float.parseFloat(animationEntry.getValue().get(4).toString());
+				spriteAnimations.put(animationEntry.getKey(), new Animation(frameDelay, textureRegions));
+			
+			}
+			
+			creatures.put(creaturePrototype.creatureType, new Creature(new Vector2(13f,30f), spriteAnimations, 0.8f, false));
+			//player.addToWorld(world, new Vector2(40f, 60f));
+			
+			//map.addCreature(player);
+			
+			
+			//System.out.println(creaturePrototype.frames.get(AnimationState.Idle));
 		}
-		
-		//gson.fromJson(new FileHandler, classOfT)
-		
-		//reader.parse(new FileHandle(directory + "/creatures.json"));
-		//reader.parse(new FileHandle("assets/creatures/creatures.json"));
-		
 		
 	}
 	
