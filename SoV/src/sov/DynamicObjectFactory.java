94af61292cde5fbcb494a568ca7c89c1f167d705
Joonas Jussila <Joonas Jussila@JoonasJussila>
2012-02-03 20:40:57 +0200

    modified sorceress' ranged attack flightspeed

--- SoV/src/sov/DynamicObjectFactory.java
+++ SoV/src/sov/DynamicObjectFactory.java
@@ -120,7 +120,7 @@ public class DynamicObjectFactory {
 					SpriteComponent.AnimationState animation = SpriteComponent.AnimationState.valueOf(attackEntry.getKey().toString());
 					
 					BodyComponent attackBodyComponent = new BodyComponent(attackComponent.parent,
-							new Vector2(20,20), false, 1.0f, false, SlopeShape.Even, false);
+							new Vector2(20,20), false, 1.0f, false, SlopeShape.Even, true);
 					//bodyComponent = parent.getComponent(BodyComponent.class);
 					
 					Attack attack;
